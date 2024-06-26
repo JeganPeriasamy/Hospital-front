dfcd36fd38aa9e5edf751b07d837cf04453cdd33
JEGAN PERIASAMY <166896131+JeganPeriasamy@users.noreply.github.com>
2024-06-26 20:25:29 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -11,7 +11,7 @@ const Navbar = () => {
 
   const handleLogout = async () => {
     await axios
-      .get("http://localhost:4000/api/v1/user/patient/logout", {
+      .get("https://hospital-back-99kc.onrender.com/api/v1/user/patient/logout", {
         withCredentials: true,
       })
       .then((res) => {
